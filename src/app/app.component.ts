64738a6596626c2e1e51b7f529e93dd661e2372c
rakhatsatybaldy <rahat_05_01@mail.ru>
2020-09-27 20:55:53 +0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  mainTitle = 'Welcome to SERVICE';
+  secTitle = 'where you can search hotels in Kazakhstan';
+}
